881a584f2c73d045c7855cea5e76c33f2231bb6d
lyc <3090487933@qq.com>
2025-01-07 10:57:09 +0800

    First commit

---
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.sunnyweather.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sunnyweather.android.databinding.ActivityMainBinding
+import com.sunnyweather.android.ui.theme.SunnyWeatherTheme
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
